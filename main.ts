1423dd71c8762e7bb4ffcf2a1d4cd3a024e83f36
MiaomiaoWang0129 <147887532+MiaomiaoWang0129@users.noreply.github.com>
2023-10-14 10:34:35 +1100

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,95 @@
+namespace SpriteKind {
+    export const NPC1 = SpriteKind.create()
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.NPC1, function (sprite, otherSprite) {
+    game.showLongText("Chinese characters are one of the only ancient scripts still in use in the world and have a history of thousands of years.", DialogLayout.Top)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleInsignia, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+let mySprite = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 100)
+tiles.setTilemap(tilemap`级别1`)
+tiles.placeOnRandomTile(mySprite, sprites.dungeon.collectibleRedCrystal)
+scene.cameraFollowSprite(mySprite)
+info.startCountdown(500)
+let NPC2 = sprites.create(img`
+    ........................
+    ........................
+    ........................
+    ........................
+    ..........ffff..........
+    ........ff1111ff........
+    .......fb111111bf.......
+    .......f11111111f.......
+    ......fd11111111df......
+    ......fd11111111df......
+    ......fddd1111dddf......
+    ......fbdbfddfbdbf......
+    ......fcdcf11fcdcf......
+    .....ffff111111bf.......
+    ....fc111cdb1bdfff......
+    ....f1b1bcbfbfc111cf....
+    ....fbfbfbffff1b1b1f....
+    .........fffffffbfbf....
+    ..........fffff.........
+    ...........fff..........
+    ........................
+    ........................
+    ........................
+    ........................
+    `, SpriteKind.NPC1)
+tiles.placeOnRandomTile(NPC2, sprites.dungeon.chestOpen)
+let NPC3 = sprites.create(img`
+    e e e . . . . e e e . . . . 
+    c d d c . . c d d c . . . . 
+    c b d d f f d d b c . . . . 
+    c 3 b d d b d b 3 c . . . . 
+    f b 3 d d d d 3 b f . . . . 
+    e d d d d d d d d e . . . . 
+    e d f d d d d f d e . b f b 
+    f d d f d d f d d f . f d f 
+    f b d d b b d d 2 f . f d f 
+    . f 2 2 2 2 2 2 b b f f d f 
+    . f b d d d d d d b b d b f 
+    . f d d d d d b d d f f f . 
+    . f d f f f d f f d f . . . 
+    . f f . . f f . . f f . . . 
+    `, SpriteKind.NPC1)
+tiles.placeOnRandomTile(NPC3, sprites.dungeon.greenOuterSouth2)
+let NPC4 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.NPC1)
+tiles.placeOnRandomTile(NPC4, sprites.dungeon.greenOuterSouth2)
---
+++ tilemap.g.ts
@@ -0,0 +1,114 @@
+// 自动生成的代码。请勿编辑。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level_0":
+            case "level_0":return tiles.createTilemap(hex`100010000102020c0c1801020c0202180c0202030a0f0b0b0b0b090b0b0b0b0b0b0b0f17000b0b0b0b0b090b0b0b0b0f0b0b0b0d090b0b010c0c1b0f0b1a0707100b0b0d0a0b0b090b0b0b0b0b0d0f0b090b0b04090f0b0a0b0b0f0b0b0d0b0b0a0b0b04120b0b0a0b0b090f0b0d0b0f090f0b170a0b0b08100f090b0b0d0f0b120b0b0d090b0f0b0806090b0b0d0b0b0a0b0b040a0b0b0b0f0b090f0b0d0b0b090f0b0d0a0b0b0b0b0b090b0b0d0b0b090b0b04090b0b0c0c0c1b0b0f0b0b0f090b0b04120b0b0b0b0b0f0b0b0b0b0b090b0b170a0b0b0b0b0b090b0b0b0b0b0a0b0004090f0b0b0b0f090b0b0f0b0b090f0b0408070719070608060719060608060705`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.floorDark2,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterEast1,sprites.dungeon.floorDark1,sprites.dungeon.floorDark5,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.greenOuterWest2,sprites.dungeon.stairWest,sprites.dungeon.stairEast,sprites.dungeon.stairLarge,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerSouthEast,myTiles.transparency16], TileScale.Sixteen);
+            case "level_1":
+            case "level_1":return tiles.createTilemap(hex`100010000102020c0c1801020c0202180c0202030a0f0b0b0b0b090b0b0b0b0b0b0b0f17130b0b0b0b0b090b0b0b0b0f0b0b0b0d090b0b010c0c1b0f0b1a0707100b0b0d0a0b0b090b0b0b0b0b0d0f0b090b0b04090f0b0a0b0b0f0b0b0d0b0b0a0b0b04120b0b0a0b0b090f0b0d0b0f090f0b170a0b0b08100f090b0b0d0f0b120b0b0d090b0f0b0806090b0b0d0b0b0a0b0b040a0b0b0b0f0b090f0b0d0b0b090f0b0d0a0b0b0b0b0b090b0b0d0b0b090b0b04090b0b0c0c0c1b0b0f0b0b0f090b0b04120b0b0b0b0b0f0b0b0b0b0b090b0b170a0b0b0b0b0b090b0b0b0b0b0a0b1c04090f0b0b0b0f090b0b0f0b0b090f0b0408070719070608060719060608060705`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterWest0,sprites.dungeon.floorDark2,sprites.dungeon.greenOuterNorth1,sprites.dungeon.greenOuterEast1,sprites.dungeon.floorDark1,sprites.dungeon.floorDark5,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.greenOuterWest2,sprites.dungeon.stairWest,sprites.dungeon.stairEast,sprites.dungeon.stairLarge,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.stairLadder], TileScale.Sixteen);
+            case "级别2":
+            case "级别2":return tiles.createTilemap(hex`14001400000008080000000000000001010401010000000000000100000008040000000104000401000000000000090009000000000000010a000a0100000000000009000701080700080001000000010000000000000700000000000008000100010001000000000000000000090004000000010004000400000000000009070701000101000501000400010000000000000001070101010704040400010001000000000000000100000000000000000004000100000000000000070007010901010100040a0004000000000000000100010000000000000000040100000000000000000000000009000400040004010000000000000001070901000000000004000401000000000000000004000000040000000400040400000000000004000000040000000000000006010000000000000107010101000101000401010404000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+2222222222222222....
+..2...22.2222222....
+2.2.2....2222222....
+2.2.2222.222...2....
+2.2......222.2.2....
+2....2.2..22.2.2....
+2.2222.22.22.2.2....
+2..222222222.2.2....
+22.2.........2.2....
+22.2.222222.22.2....
+22.2.2........22....
+2.......2.2.2.22....
+22.2222222..2.22....
+2...2...2...2.22....
+2.2...2...2....2....
+2222222222222222....
+....................
+....................
+....................
+....................
+`, [myTiles.transparency16,sprites.dungeon.floorLight0,sprites.dungeon.floorLightMoss,sprites.dungeon.collectibleInsignia,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.floorLight3,sprites.dungeon.floorDark1,sprites.dungeon.floorMixed,sprites.dungeon.floorDark3], TileScale.Sixteen);
+            case "级别1":
+            case "级别1":return tiles.createTilemap(hex`1e001e0001010101010101010101010101010101010101010701010101010101010105030308030307070404070404040404040404040707020204040407070101070707070303030704040407070707070707040404040404040704040404040404040707030707070707070704080404070704070707070707070404010707070707090704040407070404070704040704070404080404070404070303030707030704070407070407070704040407040407070704070404070a070307020d070e0704070704070707070704070407070707040701040703070a070703070407040707040404040407040704070404070407010407030703070709070407040707040404040407100704070404040407040407030703030703070407040707040707070707040404070407070707040407040307030703070a07040407040408040407070707070a07040404010404070307030709070407070407070707070407040407070407040704010104070c07030703070407070407070707070407040407070407040704010104070307030303070407070401040408040407040411040407040704010404070a0707020704040707040704070707070704070707070704070401040707040704040404070707040704040404070704070704040404070a0101040404070407070707070404070707070407040407070401070707040101040707070407040404070401070707070407040707070412120412040101090707070404040704070401040804070f070a07070404070404130401010407070701010707040704010407040704070404040407070407070704010907040404040707040704010407040704070707070704070404040701010407070707070404040704010407040404070707070404040707110704010404040404040407070704010a07070704040404040704070404040701040707070707070707040404010404040404040407040704070404040401010404040404040407040707070707070707070707040707070707070401040707070707070707040704040404040404040707040404040404070a01010404080404040804040704040404040707040707070707070704070401010b07070707070707070707070707040707040408040404040404070401010404040804080408040408040804040707070707070707070707070601010101010101010101010101010101010101010101010101010101010101`, img`
+222222222222222222222222222222
+......222222222222222222222222
+22222...2222222222222222222222
+2222222.2222222....22222222222
+2222222.2...22..22..222.....22
+22...22.2.2.22.222...2..222.22
+22.2.22.2.2.22.22222.2.2222.22
+22.2.22.2.2.22.....2.2.2..2.22
+22.2.22.2.2.22.....2.2.2....22
+22.2..2.2.2.22.22222...2.22222
+22..2.2.2.2..2.....22222.2...2
+222.2.2.2.22.22222.2..22.2.2.2
+222.2.2.2.22.22222.2..22.2.2.2
+222.2...2.22.2.....2.....2.2.2
+222.2222..22.2.22222.22222.2.2
+222.2....222.2....22.22....2.2
+2...2.22222..2222.2..22.2222.2
+2.222.2...2.22222.2.222.22.2.2
+2.222...2.2.2...2.2.22..2....2
+2.2222222.2.2.2.2.2....22.2222
+2.2222222.2.2.2.2.2222222...22
+2.22222...2.2.2...222222222.22
+2.......222.2.222.....222...22
+222222222...2.......2.222....2
+222222222.22222222222.222222.2
+222222222.2........22......2.2
+2.........2.....22.2222222.2.2
+2.2222222222222.22.........2.2
+2...............222222222222.2
+222222222222222222222222222222
+`, [myTiles.transparency16,sprites.dungeon.floorLight1,sprites.dungeon.floorLight0,sprites.dungeon.darkGroundSouthEast1,sprites.dungeon.darkGroundCenter,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.collectibleInsignia,sprites.dungeon.floorLightMoss,sprites.dungeon.stairWest,sprites.dungeon.stairSouth,sprites.dungeon.stairNorth,sprites.dungeon.purpleSwitchUp,sprites.dungeon.greenOuterSouth2,sprites.dungeon.chestOpen,sprites.dungeon.greenOuterEast2,sprites.dungeon.stairLadder,sprites.dungeon.purpleOuterNorth2,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.floorDark4,sprites.dungeon.purpleOuterWest1], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// 自动生成的代码。请勿编辑。
